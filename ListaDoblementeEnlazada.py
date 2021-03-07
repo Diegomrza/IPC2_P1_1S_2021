fae1b22a3868e044c6b5385d622da8d846d7f265
Diegomrza <2993323220101@ingenieria.usac.edu.gt>
2021-03-07 00:25:59 -0600

    primer avance matriz reducida

--- ListaDoblementeEnlazada.py
+++ ListaDoblementeEnlazada.py
@@ -3,7 +3,7 @@ from ListaSimple import ListaSimple
 class ListaDoblementeEnlazada:
     def __init__(self):
         self.primero = None
-
+    #Método que inserta al final
     def insertar_final(self, nombre, n, m, matriz):
         nodo_nuevo = Matrices(nombre, n, m)
         nodo_nuevo.matriz = matriz
@@ -17,14 +17,14 @@ class ListaDoblementeEnlazada:
                 temporal = temporal.siguiente
             temporal.siguiente = nodo_nuevo
             nodo_nuevo.anterior = temporal
-       
+    #Método que muestra los nodos en una lista doble
     def mostrar(self):
         temporal = self.primero
         while temporal is not None:
             print(temporal.nombre, " n =", temporal.n, " m =", temporal.m)
             temporal.matriz.mostrar()
             temporal = temporal.siguiente
-        
+    #Método que comprueba si el nombre de una matriz ya existe
     def comprobar_nombre(self, nombre_a_comparar):
         aux = self.primero
         
@@ -34,10 +34,10 @@ class ListaDoblementeEnlazada:
             aux = aux.siguiente
 
         return False
-
+    #Método que comprueba si una lista está vacía
     def vacia(self):
         return self.primero == None
-
+    #Método que obtiene la matriz binaria
     def matroz(self, matriz, n, m):
 
         obj_l_p = ListaSimple()
@@ -49,5 +49,34 @@ class ListaDoblementeEnlazada:
                     val = 1
                 obj_l_p.insertar(i+1, j+1, val)
                 
-        print("\nMatriz binaria")
-        obj_l_p.mostrar()
+        """print("\nMatriz binaria")
+        obj_l_p.mostrar()"""
+
+        self.prueba(obj_l_p, n, m)
+        
+    def prueba(self,obj_l_p, n, m):
+
+        for x in range(0,n):
+            lista_temporal = ListaSimple()
+            for y in range(0,m):
+                valor = obj_l_p.pos(x+1, y+1)
+                lista_temporal.insertar(x+1,y+1,valor)
+            self.comparar_filas(lista_temporal, obj_l_p, n, m, x+1)
+        print("----------------------------------------------------------------")
+    def comparar_filas(self, fila, fila2, n, m, indice):
+        
+        print("Indice: ",indice)
+
+        for x in range(1,n+1):
+            contador = 0
+            for y in range(1, m+1):
+                if indice == x:
+                    break
+                elif fila2.obtener_nodo(x,y) == fila.obtener_nodo(indice,y):
+                    contador+=1
+                    #print("valor:\t",fila.obtener_nodo(indice,y))
+                    #print("valor2:\t",fila2.obtener_nodo(x,y))
+                    if contador == m:
+                        print("Las filas son iguales")
+
+        
--- ListaSimple.py
+++ ListaSimple.py
@@ -3,7 +3,7 @@ from NodoSimple import NodoSimple
 class ListaSimple:
     def __init__(self):
         self.primero = None
-
+    #Método que inserta al final
     def insertar(self, x, y, contenido):
         nuevo = NodoSimple(x, y, contenido)
         if self.primero is None:
@@ -13,14 +13,14 @@ class ListaSimple:
             while temporal.siguiente is not None:
                 temporal = temporal.siguiente
             temporal.siguiente = nuevo
-
+    #Método que muestra los elementos de una lista matriz
     def mostrar(self):
         tmp = self.primero
         while tmp is not None:
             print('ccx: ',  tmp.x, '\tccy: ', tmp.y, '\tContenido: ', tmp.contenido)
             tmp = tmp.siguiente
         print("")
-    
+    #Método que obtiene un valor por medio de sus coordenadas su posición
     def pos(self, fila, columna):
         aux = self.primero
         while aux != None:
@@ -29,3 +29,17 @@ class ListaSimple:
             else:
                 aux = aux.siguiente
         return None
+    def obtener_nodo(self,x,y):
+        aux = self.primero
+        while aux != None:
+            if aux.x == x and aux.y == y:
+                return aux.contenido
+            aux = aux.siguiente
+
+    def obtener_tamaño(self):
+        aux = self.primero
+        contador = 0
+        while aux != None:
+            contador += 1
+            aux = aux.siguiente
+        return contador 
